128a76f9c0db5b4aa3f3d15e20abcc25d96689e5
hina <hina@hinadeMacBook-Air.local>
2025-08-10 21:59:04 +0800

    adjust env name

--- RAGPilot/settings.py
+++ RAGPilot/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'pikachu')
+SECRET_KEY = os.getenv('SECRET_KEY', 'pikachu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
